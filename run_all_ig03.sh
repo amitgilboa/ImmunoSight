5f95749cc353ccf5f3f3a70f634f8696342a05c4
amitgilboa <58215769+amitgilboa@users.noreply.github.com>
2024-04-02 09:07:59 +0300

    Add files via upload

---
+++ run_all_ig03.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+nice -19 python /work/amitk/airrmap/tcr_anchors_cdr3/scripts/simulations_ligo_cdr3.py --server ig03 --path_rep 'repertoire_sim100_50_01'
+nice -19 python /work/amitk/airrmap/tcr_anchors_cdr3/scripts/project_tcr_anchors_cdr3.py --server ig03 --path_rep 'repertoire_sim100_50_01'
+nice -19 python /work/amitk/airrmap/tcr_anchors_cdr3/scripts/project_tcr_anchors_cdr3_mlat.py --server ig03 --path_rep 'repertoire_sim100_50_01'
+nice -19 python /work/amitk/airrmap/tcr_anchors_cdr3/scripts/classification_models_kde_cdr3_all_folds.py  --server ig03 --path_rep 'repertoire_sim100_50_01' --model_method 'kde_hist' --bins 128
